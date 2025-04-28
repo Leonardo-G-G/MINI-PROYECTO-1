655675807c1ce4cc372c090bc79100a7ab1f62bb
MGuadalupeR <l21270655@tuxtla.tecnm.mx>
2025-04-28 11:48:03 -0600

    Actualización de UpdateCategoriaRequest

--- app/Http/Requests/UpdateCategoriaRequest.php
+++ app/Http/Requests/UpdateCategoriaRequest.php
@@ -14,7 +14,7 @@ class UpdateCategoriaRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'nombre' => 'required|string|max:255|unique:categorias,nombre,' . $this->route('categoria'), // Ignora la categoría actual por ID
+            'nombre' => 'required|string|max:255|unique:categorias,nombre,' . $this->route('categoria')->id, // Aquí accedemos correctamente al ID
             'descripcion' => 'nullable|string|max:1000',
         ];
     }
